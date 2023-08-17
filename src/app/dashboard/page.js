11b0d7f67dd746a6eeaaeee7e518ee4c9e1df6b9
Younes Kazemi <youneskazemi9798@gmail.com>
2023-08-17 17:04:55 +0330

    dashboard main page

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,5 +1,15 @@
-function Dashboard() {
-  return <div>Dashboard</div>;
+import DashboardPage from "@/template/DashboardPage";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import connectDB from "@/utils/connectDB";
+import User from "@/models/User";
+
+async function Dashboard() {
+  await connectDB();
+  const session = await getServerSession(authOptions);
+
+  const user = await User.findOne({ email: session.user.email });
+  return <DashboardPage createdAt={user.createdAt} />;
 }
 
 export default Dashboard;
