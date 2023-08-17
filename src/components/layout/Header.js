ec3600774d77a53926a8ee816e6593e6b79be88b
Younes Kazemi <youneskazemi9798@gmail.com>
2023-08-17 12:44:33 +0330

    authenticate in header

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,7 +1,14 @@
+"use client";
+
 import Link from "next/link";
-import styles from "@/layout/Home.module.css";
 import { FiLogIn } from "react-icons/fi";
+import { FaUserAlt } from "react-icons/fa";
+import styles from "@/layout/Header.module.css";
+import { useSession } from "next-auth/react";
+
 function Header() {
+  const { data } = useSession();
+
   return (
     <header className={styles.header}>
       <div>
@@ -14,13 +21,20 @@ function Header() {
           </li>
         </ul>
       </div>
-
-      <div className={styles.login}>
-        <Link href="/signin">
-          <FiLogIn />
-          <span>ورود</span>
-        </Link>
-      </div>
+      {data ? (
+        <div className={styles.login}>
+          <Link href="/dashboard">
+            <FaUserAlt />
+          </Link>
+        </div>
+      ) : (
+        <div className={styles.login}>
+          <Link href="/signin">
+            <FiLogIn />
+            <span>ورود</span>
+          </Link>
+        </div>
+      )}
     </header>
   );
 }
---
+++ src/providers/NextAuthProvider.js
@@ -0,0 +1,9 @@
+"use client";
+
+import { SessionProvider } from "next-auth/react";
+
+export function NextAuthProvider({ children }) {
+  return <SessionProvider>{children}</SessionProvider>;
+}
+
+export default NextAuthProvider;
