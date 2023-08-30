5eb1bb3238dfa066d9d2b804bf6f88347644f8ff
Younes Kazemi <youneskazemi9798@gmail.com>
2023-08-30 20:37:30 +0330

    profile details page

---
+++ src/app/buy-residential/[profileId]/page.js
@@ -0,0 +1,15 @@
+import Profile from "@/models/Profile";
+import DetailsPage from "@/template/DetailsPage";
+import connectDB from "@/utils/connectDB";
+import React from "react";
+
+async function ProfileDetails({ params: { profileId } }) {
+  await connectDB();
+  const profile = await Profile.findOne({ _id: profileId });
+
+  if (!profile) return <h3>مشکلی پیش آمده است!</h3>;
+
+  return <DetailsPage data={profile} />;
+}
+
+export default ProfileDetails;
